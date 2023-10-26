90c2ecdb358c227b5a316e87a2a1c08f9ae7faca
matheus cascalho <matheus.santos@enacom.com.br>
2023-10-26 18:41:19 -0300

    teste de identificador de nó

--- tests/test_capacity_restriction.py
+++ tests/test_capacity_restriction.py
@@ -1,6 +1,8 @@
-def test_each_node_should_have_different_id():
-    assert False
+from optimizer.restrictions.capacity_restriction import CapacityRestrictions, Node, Flow
 
 
 def test_node_ids_should_follow_numeric_sequence_by_object_creation():
-    assert False
+    n1 = Node(name='terminal 1', capacity=500)
+    n2 = Node(name='terminal 2', capacity=600)
+    assert n1.identifier == 0
+    assert n2.identifier == 1
