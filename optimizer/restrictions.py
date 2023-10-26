c7dfd915d12fbd4b20be431dc32230a0e4fe57d5
matheus cascalho <matheus.santos@enacom.com.br>
2023-10-26 07:16:07 -0300

    declaração da interface das restrições

---
+++ optimizer/restrictions.py
@@ -0,0 +1,11 @@
+"""
+This file declares the restriction interface - class Restriction - and implements each restriction for
+railroad optimization problem - ROP
+
+"""
+
+import abc
+
+
+class Restriction(abc.ABC):
+    ...
