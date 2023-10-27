8c4bc34a4fc6a597559b35a8faf2b48d4707218f
matheus cascalho <matheus.santos@enacom.com.br>
2023-10-27 19:00:35 -0300

    teste para 1 origem e 2 destinos

--- tests/test_capacity_restriction.py
+++ tests/test_capacity_restriction.py
@@ -116,3 +116,40 @@ def test_restriction_vector_for_two_flows_and_one_train():
     expected = np.array(expected)
 
     np.testing.assert_allclose(actual, expected)
+
+
+def test_restriction_vector_for_one_origin_and_two_destinations_and_two_trains():
+    n1 = Node(name='terminal 1', capacity=500)
+    n2 = Node(name='terminal 2', capacity=600)
+    n3 = Node(name='terminal 3', capacity=600)
+    flows = [
+        Flow(
+            origin=n1,
+            destination=n2,
+            train_volume=50
+        ),
+        Flow(
+            origin=n1,
+            destination=n3,
+            train_volume=60
+        )
+    ]
+    constraint = CapacityRestrictions(trains=2, flows=flows)
+
+    # Act
+    actual = constraint.restrictions()[0].to_vector()
+
+    # Assert
+    expected = [
+        50,      # t1->n2->n1->n2
+        60,      # t1->n2->n1->n3
+        50,      # t1->n3->n1->n2
+        60,      # t1->n3->n1->n3
+        50,      # t2->n2->n1->n2
+        60,      # t2->n2->n1->n3
+        50,      # t2->n3->n1->n2
+        60,      # t2->n3->n1->n3
+    ]
+    expected = np.array(expected)
+
+    np.testing.assert_allclose(actual, expected)
