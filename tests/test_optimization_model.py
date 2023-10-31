d6ae711209102c555d51397b8a311e340a826cbd
matheus cascalho <matheus.santos@enacom.com.br>
2023-10-31 09:10:42 -0300

    declaração de testes do problema

---
+++ tests/test_optimization_model.py
@@ -0,0 +1,18 @@
+def test_problem_should_have_2_capacity_constraints():
+    ...
+
+
+def test_problem_should_have_2_time_horizon_constraints():
+    ...
+
+
+def test_problem_should_have_3_maximum_demand_constraints():
+    ...
+
+
+def test_problem_should_have_3_minimum_demand_constraints():
+    ...
+
+
+def test_problem_time_horizon_restriction_should_have_coefficient_just_for_specific_train():
+    ...
