6678faa737984b9e7de2757472f6b12ce6967191
matheus cascalho <matheus.santos@enacom.com.br>
2023-11-05 23:30:43 -0300

    adição dos tempos de processamento na restrição de horizonte de tempo

--- optimizer/restrictions/time_horizon_restriction.py
+++ optimizer/restrictions/time_horizon_restriction.py
@@ -15,19 +15,34 @@ class TimeHorizonRestriction(RailroadProblemTemplate, Restrictions):
             trains=trains,
             flows=flows
         )
-        self.__restrictions = self.__build_restrictions(transit_times=transit_times, time_horizon=time_horizon)
+        self.__restrictions = self.__build_restrictions(
+            transit_times=transit_times,
+            time_horizon=time_horizon,
+            flows=flows
+        )
 
-    def __build_restrictions(self, transit_times: list[TransitTime], time_horizon: int) -> list[Restriction]:
+    def __build_restrictions(
+            self,
+            transit_times: list[TransitTime],
+            time_horizon: int,
+            flows: list[Flow]
+    ) -> list[Restriction]:
         restrictions = []
         transit_matrix = self.__build_transit_matrix(transit_times=transit_times)
+        train_volumes = self.__build_train_volumes(flows=flows)
         self.transit_matrix = transit_matrix
         for n in range(self.cardinality[0]):
             matrix = np.zeros(self.cardinality)
-            for j in range(len(self.loaded_origins)):
-                for k in range(len(self.loaded_destinations)):
+            for j, load_point in enumerate(self.loaded_origins):
+                for k, unload_destination in enumerate(self.loaded_destinations):
                     time = transit_matrix[j, k]
-                    matrix[n, :, j, k] += time
-                    matrix[n, k, j, :] += time
+                    train_volume = train_volumes[j, k]
+                    load_rate = load_point.capacity / time_horizon
+                    load_process = train_volume / load_rate
+                    unload_rate = unload_destination.capacity / time_horizon
+                    unload_process = train_volume / unload_rate
+                    matrix[n, :, j, k] += time + unload_process
+                    matrix[n, k, j, :] += time + load_process
             restriction = Restriction(
                 coefficients=matrix,
                 sense=self.restriction_type.value,
@@ -46,6 +61,17 @@ class TimeHorizonRestriction(RailroadProblemTemplate, Restrictions):
                 j = self.loaded_origins.index(transit.origin)
                 k = self.loaded_destinations.index(transit.destination)
                 matrix[j, k] = transit.time
+        return matrix
+
+    def __build_train_volumes(self, flows):
+        l = len(self.loaded_origins)
+        u = len(self.loaded_destinations)
+        shape = (l, u)
+        matrix = np.zeros(shape)
+        for flow in flows:
+            i = self.loaded_origins.index(flow.origin)
+            j = self.loaded_destinations.index(flow.destination)
+            matrix[i, j] = flow.train_volume
         return matrix
 
     def restrictions(self) -> list[Restriction]:
